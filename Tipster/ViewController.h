4a9deae815d1e3e626574f6e2fb06fcf02dbbaff
Ernest Omondi <eomondi@fb.com>
2018-06-26 13:32:16 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Ernest Omondi on 6/26/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
